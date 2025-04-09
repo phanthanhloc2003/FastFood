b45375e8ba990fae12cb8690ac55af78bcfb5099
Phan thanh Lộc <phanthanhloc@MacBook-Pro-cua-Phan.local>
2025-04-09 22:47:33 +0700

    [config:be] connet db

--- my-nest-ts-app/src/db/database.providers.ts
+++ my-nest-ts-app/src/db/database.providers.ts
@@ -6,13 +6,19 @@ export const databaseProviders = [
     useFactory: async () => {
       const dataSource = new DataSource({
         type: 'postgres',
-        host: 'localhost',
+        host: 'dpg-cvr97kbuibrs73ae6hr0-a.oregon-postgres.render.com',
         port: 5432,
-        username: 'postgres',
-        password: 'Ptl2003.',
-        database: 'postgres',
+        username: 'fastfood_db_user', 
+        password: 'kkwSIqOcanvEPucI61aWo6m5kYVPLXcj', 
+        database: 'fastfood_db', 
         entities: [__dirname + '/../**/*.entity{.ts,.js}'],
         synchronize: true,
+        ssl: true, 
+        extra: {
+          ssl: {
+            rejectUnauthorized: false 
+          }
+        }
       });
       return dataSource.initialize();
     },
