16a4d2699d98a4649acb8321cdec925e6b6a35ea
Phan thanh Lộc <phanthanhloc@MacBook-Pro-cua-Phan.local>
2025-04-22 17:48:19 +0700

    [feate:be] change link db

--- my-nest-ts-app/src/db/database.providers.ts
+++ my-nest-ts-app/src/db/database.providers.ts
@@ -1,30 +1,3 @@
-// import { DataSource } from 'typeorm';
-
-// export const databaseProviders = [
-//   {
-//     provide: 'DATA_SOURCE',
-//     useFactory: async () => {
-//       const dataSource = new DataSource({
-//         type: 'postgres',
-//         host: 'dpg-cvr97kbuibrs73ae6hr0-a.oregon-postgres.render.com',
-//         port: 5432,
-//         username: 'fastfood_db_user', 
-//         password: 'kkwSIqOcanvEPucI61aWo6m5kYVPLXcj', 
-//         database: 'fastfood_db', 
-//         entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-//         synchronize: true,
-//         ssl: true, 
-//         extra: {
-//           ssl: {
-//             rejectUnauthorized: false 
-//           }
-//         }
-//       });
-//       return dataSource.initialize();
-//     },
-//   },
-// ];
-
 import { DataSource } from 'typeorm';
 
 export const databaseProviders = [
@@ -33,15 +6,42 @@ export const databaseProviders = [
     useFactory: async () => {
       const dataSource = new DataSource({
         type: 'postgres',
-        host: 'localhost',
+        host: 'dpg-cvr97kbuibrs73ae6hr0-a.oregon-postgres.render.com',
         port: 5432,
-        username: 'postgres', 
-        password: 'Ptl2003.', 
-        database: 'postgres', 
+        username: 'fastfood_db_user', 
+        password: 'kkwSIqOcanvEPucI61aWo6m5kYVPLXcj', 
+        database: 'fastfood_db', 
         entities: [__dirname + '/../**/*.entity{.ts,.js}'],
         synchronize: true,
+        ssl: true, 
+        extra: {
+          ssl: {
+            rejectUnauthorized: false 
+          }
+        }
       });
       return dataSource.initialize();
     },
   },
 ];
+
+// import { DataSource } from 'typeorm';
+
+// export const databaseProviders = [
+//   {
+//     provide: 'DATA_SOURCE',
+//     useFactory: async () => {
+//       const dataSource = new DataSource({
+//         type: 'postgres',
+//         host: 'localhost',
+//         port: 5432,
+//         username: 'postgres', 
+//         password: 'Ptl2003.', 
+//         database: 'postgres', 
+//         entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+//         synchronize: true,
+//       });
+//       return dataSource.initialize();
+//     },
+//   },
+// ];
